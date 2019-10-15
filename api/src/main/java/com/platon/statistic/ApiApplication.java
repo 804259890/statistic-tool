81a14dd1053b1a7b78e5c7584dfd0f1e92dc4223
chendongming <chendongming@juzix.net>
2019-10-15 13:56:13 +0800

    调整结构

--- api/src/main/java/com/platon/statistic/ApiApplication.java
+++ api/src/main/java/com/platon/statistic/ApiApplication.java
@@ -3,12 +3,14 @@ package com.platon.statistic;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-public class StatisticToolApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(StatisticToolApplication.class);
+		SpringApplication app = new SpringApplication(ApiApplication.class);
 		app.setBannerMode(Banner.Mode.OFF);
 		app.run(args);
 	}
